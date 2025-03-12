da6c7f849826ee4cf90af1103ed8b47a4a1e7c6e
-Xavi Villaverde <xavier.villaverde-lomban@capgemini.com>
2025-03-12 11:19:15 +0100

    full refactor in base code

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -3,8 +3,13 @@ package com.gildedrose;
 class GildedRose {
     private static final String CONJURED = "Conjured";
     private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+    private static final int QUALITY_CHANGE = 1;
+    private static final int CONJURED_QUALITY_CHANGE = 2;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,71 +17,64 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        if (items[i].name.startsWith(CONJURED)) {
-                            items[i].quality = items[i].quality - 2;
-                            if  (items[i].quality < 0) {
-                                items[i].quality = 0;
-                            }
-
-                        } else {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                if (items[i].name.contains(CONJURED)) {
-                                    
-                                    items[i].quality = items[i].quality - 2;
-                                    if  (items[i].quality < 0) {
-                                        items[i].quality = 0;
-                                    }
-                                } else {
-                                    items[i].quality = items[i].quality - 1;
-                                }
-                            }
-                        }
-                    } else {
-                        items[i].quality = 0;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        for (Item item : items) {
+            updateItemQuality(item);
+            updateItemSellIn(item);
+            handleExpiredItem(item);
         }
     }
-}
+
+    private void updateItemQuality(Item item) {
+        if (item.name.equals(AGED_BRIE)) {
+            increaseQuality(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES)) {
+            updateBackstagePassesQuality(item);
+        } else if (!item.name.equals(SULFURAS)) {
+            decreaseQuality(item);
+        }
+    }
+
+    private void updateBackstagePassesQuality(Item item) {
+        increaseQuality(item);
+        if (item.sellIn < 11) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 6) {
+            increaseQuality(item);
+        }
+    }
+
+    private void updateItemSellIn(Item item) {
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn--;
+        }
+    }
+
+    private void handleExpiredItem(Item item) {
+        if (item.sellIn >= 0) {
+            return;
+        }
+
+        if (item.name.equals(AGED_BRIE)) {
+            increaseQuality(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES)) {
+            item.quality = MIN_QUALITY;
+        } else if (!item.name.equals(SULFURAS)) {
+            decreaseQuality(item);
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.name.startsWith(CONJURED)) {
+            item.quality = Math.max(item.quality - CONJURED_QUALITY_CHANGE, MIN_QUALITY);
+        } else {
+            item.quality = Math.max(item.quality - QUALITY_CHANGE, MIN_QUALITY);
+        }
+    }
 }
--- GildedRose-Refactoring-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,10 +1,7 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
