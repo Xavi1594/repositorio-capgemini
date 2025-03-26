6a11d0a9305d8920f25b9e679cde9ec3d203b73e
Villaverde Lombán <xavier.villaverde-lomban@capgemini.com>
2025-03-26 08:23:14 +0100

    soap country

---
+++ countrysoap/src/main/java/com/example/productingwebservice/CountrysoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.productingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrysoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrysoapApplication.class, args);
+	}
+
+}
