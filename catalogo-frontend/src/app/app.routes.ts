f19ac0dacf37db40905fcb873b38375f70c2820f
Villaverde Lombán <xavi9415@hotmail.com>
2025-03-31 15:54:15 +0200

    get categories and initial style

--- catalogo-frontend/src/app/app.routes.ts
+++ catalogo-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { CategoryListComponent } from './category/category-list/category-list.component';
 
 export const routes: Routes = [
   {path: '', redirectTo:'/films', pathMatch:'full' },
-  {path: 'films', loadComponent:()=>import('../app/film/film-list/film-list.component').then(m=>m.FilmListComponent)}
+  {path: 'films', loadComponent:()=>import('../app/film/film-list/film-list.component').then(m=>m.FilmListComponent)},
+  {path: 'categories', loadComponent:()=>import('../app/category/category-list/category-list.component').then(m=>m.CategoryListComponent)},
 ];
