2ea62fbf367d7dda3f2995570ef94d174b9d9d41
Villaverde Lombán <xavi9415@hotmail.com>
2025-04-01 15:39:28 +0200

    film details dto update with json property

--- catalogo/src/main/java/com/example/catalogo/domains/entities/models/FilmDetailsDTO.java
+++ catalogo/src/main/java/com/example/catalogo/domains/entities/models/FilmDetailsDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.example.catalogo.domains.entities.Film;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 //import io.swagger.v3.oas.annotations.media.Schema;
 //import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
@@ -15,18 +16,22 @@ import lombok.Value;
 public class FilmDetailsDTO {
     // @Schema(description = "Identificador de la pelicula", accessMode =
     // AccessMode.READ_ONLY)
+    @JsonProperty("id")
     private int filmId;
     // @Schema(description = "Una breve descripción o resumen de la trama de la
     // película")
+    @JsonProperty("descripcion")
     private String description;
     // @Schema(description = "La duración de la película, en minutos")
     private Integer length;
     // @Schema(description = "La clasificación por edades asignada a la película",
     // allowableValues = {"G", "PG", "PG-13", "R", "NC-17"})
     private String rating;
+    @JsonProperty("valoracion")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy")
     // @Schema(description = "El año en que se estrenó la película")
     private Short releaseYear;
+    @JsonProperty("lanzamiento")
     // @Schema(description = "La duración del período de alquiler, en días")
     private Byte rentalDuration;
     // @Schema(description = "El coste de alquilar la película por el período
@@ -36,16 +41,21 @@ public class FilmDetailsDTO {
     // devuelve o se devuelve en un estado dañado")
     private BigDecimal replacementCost;
     // @Schema(description = "El título de la película")
+    @JsonProperty("titulo")
     private String title;
     // @Schema(description = "El idioma de la película")
+    @JsonProperty("idioma")
     private String language;
     // @Schema(description = "El idioma original de la película")
+    @JsonProperty("idiomaVo")
     private String languageVO;
     // @Schema(description = "Contenido Adicional")
     private List<String> specialFeatures;
     // @Schema(description = "La lista de actores que participan en la película")
+    @JsonProperty("actores")
     private List<String> actors;
     // @Schema(description = "La lista de categorías asignadas a la película")
+    @JsonProperty("categorias")
     private List<String> categories;
 
     public static FilmDetailsDTO from(Film source) {
