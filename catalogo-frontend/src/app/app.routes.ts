edbfd834a85879ea5328b57de4b0c22a0a174424
Villaverde Lombán <xavi9415@hotmail.com>
2025-04-02 13:41:48 +0200

    new-film view without funcionality

--- catalogo-frontend/src/app/app.routes.ts
+++ catalogo-frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const routes: Routes = [
   {path: 'categories', loadComponent:()=>import('../app/category/category-list/category-list.component').then(m=>m.CategoryListComponent)},
   {path: 'actors', loadComponent:()=>import('../app/actor/actor-list/actor-list.component').then(m=>m.ActorListComponent)},
   {path: 'actors/:id', loadComponent:()=>import ('../app/actor/actor-detail/actor-detail.component').then(m=>m.ActorDetailComponent)},
-  {path: 'films/:id', loadComponent:()=>import('../app/film/film-detail/film-detail.component').then(m=>m.FilmDetailComponent)}
+  {path: 'films/:id', loadComponent:()=>import('../app/film/film-detail/film-detail.component').then(m=>m.FilmDetailComponent)},
+  {path: 'films/new', loadComponent: ()=>import('../app/film/film-create/film-create.component').then(m=>m.FilmCreateComponent)}
 
 ];
---
+++ catalogo-frontend/src/app/film/film-create/film-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-create',
+  imports: [],
+  templateUrl: './film-create.component.html',
+  styleUrl: './film-create.component.css'
+})
+export class FilmCreateComponent {
+
+}
